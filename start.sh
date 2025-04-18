842db493a6958e526a4e4153059f148a58883b43
h1yan <yanpenabr@gmail.com>
2025-04-18 17:01:58 -0300

    feat: update script start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-echo "Starting n8n uptime monitor..."
+
+echo "🔧 Fixing permissions for ./data..."
+sudo chown -R 1000:1000 ./data
+
+echo "🐳 Starting n8n with Docker Compose..."
 docker compose up -d
+
+echo "✅ n8n is running at: http://localhost:6060"
